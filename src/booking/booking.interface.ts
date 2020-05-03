35d600e66f3277e5e031c45b7bde58335fd8ffeb
achan <achan@achans-MacBook-Air.local>
2020-05-03 17:46:42 +0700

    dumm data json

--- src/booking/booking.interface.ts
+++ src/booking/booking.interface.ts
@@ -1,7 +1,12 @@
+import { Technician } from "src/branch/interface/branch-technicians.interface";
+import { BookingCar } from "./interface/booking-car.interface";
+
 export class BookingInterface {
-    date:string;
-    branch:string;
-    start:number; // Unix timestamp , independent local zone
-    end:number;
-    
+    date: string;
+    branch: string;
+    start: number; // Unix timestamp , independent local zone
+    end: number;
+    slot: number;
+    technicians: Technician[]
+    car:BookingCar
 }
---
+++ src/booking/interface/booking-car.interface.ts
@@ -0,0 +1,7 @@
+export interface BookingCar {
+    brand: string;
+    model: string;
+    year: number;
+    type: string;
+    numberPlate: string;
+}
